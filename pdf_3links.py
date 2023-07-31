c7beb7e6ffafcecf34da61d5def283480a513eb3
Bruno Savoca Albors <98908114+brunosavoca@users.noreply.github.com>
2023-07-31 18:58:36 -0400

    Update pdf_3links.py

--- pdf_3links.py
+++ pdf_3links.py
@@ -8,39 +8,6 @@ from io import BytesIO
 # Esto es para el sidebar, donde el usuario ingresa la clave de la API de OpenAI
 openai.api_key = st.sidebar.text_input("Introduce tu OpenAI API Key")
 
-# Aquí es donde el usuario ingresará las URLs
-url1 = st.text_input("Introduce la URL 1")
-url2 = st.text_input("Introduce la URL 2")
-url3 = st.text_input("Introduce la URL 3")
-
-def get_content_from_url(url):
-    page = requests.get(url)
-    soup = BeautifulSoup(page.content, 'html.parser')
-    return soup.text
-
-def generate_article(url1, url2, url3):
-    content1 = get_content_from_url(url1)
-    content2 = get_content_from_url(url2)
-    content3 = get_content_from_url(url3)
-
-    combined_content = content1 + " " + content2 + " " + content3
-
-    response = openai.Completion.create(
-      engine="gpt-3.5-turbo",
-      prompt=combined_content,
-      temperature=0.1,
-      max_tokens=5000
-    )
-    
-    return response.choices[0].text.strip()
-
-if st.button('Generar artículo'):
-    if url1 and url2 and url3 and openai.api_key:
-        article = generate_article(url1, url2, url3)
-        st.write(article)
-    else:
-        st.write("Por favor, asegúrate de haber ingresado todas las URLs y tu OpenAI API Key.")
-
 # Interacción con PDFs
 st.header('Interactuar con PDFs')
 
